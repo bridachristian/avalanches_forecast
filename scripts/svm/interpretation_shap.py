b5816361d1a31b15cbfcf40b3bb098dc864eaae2
Christian Brida <bridachristian@gmail.com>
2025-05-07 14:47:16 +0200

    create new file to interpret results using shap

---
+++ scripts/svm/interpretation_shap.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  7 14:44:30 2025
+
+@author: Christian
+"""
+import time
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
+from sklearn import svm
+from sklearn.feature_selection import RFE
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import FunctionTransformer
+# from sklearn.pipeline import Pipeline
+from imblearn.pipeline import Pipeline  # Use imblearn's Pipeline
+from imblearn.under_sampling import NearMiss, ClusterCentroids
+
+from scripts.svm.data_loading import load_data
+from scripts.svm.undersampling_methods import (undersampling_random, undersampling_random_timelimited,
+                                               undersampling_nearmiss, undersampling_cnn,
+                                               undersampling_enn, undersampling_clustercentroids,
+                                               undersampling_tomeklinks)
+from scripts.svm.oversampling_methods import oversampling_random, oversampling_smote, oversampling_adasyn, oversampling_svmsmote
+from scripts.svm.svm_training import cross_validate_svm, tune_train_evaluate_svm, train_evaluate_final_svm
+from scripts.svm.evaluation import (plot_learning_curve, plot_confusion_matrix,
+                                    plot_roc_curve, permutation_ranking, evaluate_svm_with_feature_selection)
+from scripts.svm.utils import (save_outputfile, get_adjacent_values, PermutationImportanceWrapper,
+                               remove_correlated_features, remove_low_variance, select_k_best)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+from scipy.interpolate import UnivariateSpline
+
+
+if __name__ == '__main__':
+    # --- PATHS ---
+
+    # Filepath and plot folder paths
+    common_path = Path(
+        'C:\\Users\\Christian\\OneDrive\\Desktop\\Family\\Christian\\MasterMeteoUnitn\\Corsi\\4_Tesi\\03_Dati\\MOD1_manipulation\\')
+
+    filepath = common_path / 'mod1_newfeatures_NEW.csv'
+    # filepath = common_path / 'mod1_certified.csv'
+    results_path = Path(
+        'C:\\Users\\Christian\\OneDrive\\Desktop\\Family\\Christian\\MasterMeteoUnitn\\Corsi\\4_Tesi\\03_Dati\\SVM_results\\')
+
+    # --- DATA IMPORT ---
+
+    # Load and clean data
+    mod1 = load_data(filepath)
+    print(mod1.dtypes)  # For initial data type inspection
+
+    # -------------------------------------------------------
+    # STABILITY VARYING C AND GAMMA
+    # -------------------------------------------------------
+    SHAP_16 = ['TaG_delta_5d',
+               'TminG_delta_3d',
+               'HS_delta_5d',
+               'WetSnow_Temperature',
+               'New_MF_Crust',
+               'Precip_3d',
+               'Precip_2d',
+               'TempGrad_HS',
+               'Tsnow_delta_3d',
+               'TmaxG_delta_3d',
+               'HSnum',
+               'TempAmplitude_2d',
+               'WetSnow_CS',
+               'TaG',
+               'Tsnow_delta_2d',
+               'DayOfSeason']
+    res_shap16 = evaluate_svm_with_feature_selection(mod1, SHAP_16)
