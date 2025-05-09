fix a bug on feature scaling for nearmiss undersampling

scripts/svm/evaluation.py
@@ -18,9 +18,11 @@ from scripts.svm.oversampling_methods import oversampling_random, oversampling_s
 from scripts.svm.svm_training import cross_validate_svm, tune_train_evaluate_svm, train_evaluate_final_svm
 from scripts.svm.utils import get_adjacent_values, save_outputfile, remove_correlated_features
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import make_scorer, matthews_corrcoef
+from scripts.svm.feature_engineering import transform_features
 
 
-def plot_learning_curve(clf, X, y, title, cv=5):
+def plot_learning_curve(clf, X, y, title, cv=5, scoring='f1'):
     """
     Plots the learning curve for the given classifier.
 
@@ -28,26 +30,32 @@ def plot_learning_curve(clf, X, y, title, cv=5):
     - clf: Trained classifier (e.g., SVM model)
     - X: Feature data
     - y: Target labels
-    - cv: Number of cross-validation folds (default is 10)
+    - title: Title of the plot
+    - cv: Number of cross-validation folds (default is 5)
+    - scoring: Scoring metric (e.g., 'accuracy', 'f1', or a custom scorer)
 
     Returns:
     - None (displays a plot)
     """
-    # Compute learning curve
-    train_sizes, train_scores, val_scores = learning_curve(clf, X, y, cv=cv)
+    if scoring == 'mcc':
+        scorer = make_scorer(matthews_corrcoef)
+    else:
+        scorer = scoring
 
-    # Calculate mean and standard deviation of training and validation scores
+    train_sizes, train_scores, val_scores = learning_curve(
+        clf, X, y, cv=cv, scoring=scorer)
+
     train_mean = train_scores.mean(axis=1)
     val_mean = val_scores.mean(axis=1)
 
-    # Plot the learning curve
     plt.plot(train_sizes, train_mean, label="Training Score")
     plt.plot(train_sizes, val_mean, label="Validation Score")
     plt.xlabel("Training Set Size")
-    plt.ylabel("Score")
+    plt.ylabel(f"{scoring} Score")
     plt.ylim(0, 1.1)
     plt.legend()
     plt.title(f'Learning Curve - {title}')
+    plt.grid(True)
     plt.show()
 
 
@@ -222,77 +230,50 @@ def evaluate_svm_with_feature_selection(data, feature_list):
         - 'best_params': Best C and gamma values from the final cross-validation.
     """
 
-    # Add target variable to the feature list
-    feature_with_target = feature_list + ['AvalDay']
+    feature_plus = feature_list + ['AvalDay']
+    data_clean = data[feature_plus]
+    data_clean = data_clean.dropna()
+    data_transformed = transform_features(data_clean.copy())
 
-    # Data preprocessing: filter relevant features and drop missing values
-    clean_data = data[feature_with_target].dropna()
-
-    # Extract features and target variable
-    X = clean_data[feature_list]
-    y = clean_data['AvalDay']
+    X = data_transformed[feature_list]
+    y = data_transformed['AvalDay']
 
     features_to_remove = remove_correlated_features(X, y)
 
     X = X.drop(columns=features_to_remove)
 
-    # initial_param_grid = {
-    #     'C': [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000],
-    #     'gamma': [100, 50, 10, 5, 1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
-    # }
-    initial_param_grid = {
-        'C': [
-            0.001, 0.0015, 0.002, 0.003,
-            0.005, 0.0075, 0.01, 0.015, 0.02, 0.03, 0.05, 0.075, 0.1, 0.15, 0.2, 0.3, 0.5,
-            0.75, 1, 1.5, 2, 3, 5, 7.5, 10, 15, 20, 30, 50, 75, 100, 150, 200, 300, 500,
-            750, 1000
-        ],
-        'gamma': [
-            100, 75, 50, 30, 20, 15, 10, 7.5, 5, 3, 2, 1.5, 1,
-            0.75, 0.5, 0.3, 0.2, 0.15, 0.1, 0.08, 0.07, 0.05, 0.03, 0.02, 0.015, 0.01, 0.008,
-            0.007, 0.005, 0.003, 0.002, 0.0015, 0.001, 0.0008, 0.0007, 0.0005, 0.0003, 0.0002,
-            0.00015, 0.0001
-        ]
-    }
-    # initial_param_grid = {
-    #     'C': [
-    #         0.001, 0.0015, 0.002, 0.003,
-    #         0.005, 0.0075, 0.01, 0.015, 0.02, 0.03, 0.05, 0.075, 0.1, 0.15, 0.2, 0.3, 0.5,
-    #         0.75, 1, 1.5, 2, 3, 5, 7.5, 10, 12.5, 15, 20, 25, 30, 50, 75, 100, 125, 150, 200,
-    #         250, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500
-    #     ],
-    #     'gamma': [
-    #         100, 75, 50, 30, 20, 15, 10, 7.5, 5, 3, 2, 1.5, 1,
-    #         0.75, 0.5, 0.3, 0.2, 0.15, 0.1, 0.08, 0.07, 0.05, 0.03, 0.02, 0.015, 0.01, 0.008,
-    #         0.007, 0.005, 0.003, 0.002, 0.0015, 0.001, 0.0008, 0.0007, 0.0005, 0.0003, 0.0002,
-    #         0.00015, 0.0001, 0.000075
-    #     ]
-    # }
+    param_grid = {
+        'C': [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009,
+              0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+              0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+              1, 2, 3, 4, 5, 6, 7, 8, 9,
+              10, 20, 30, 40, 50, 60, 70, 80, 90,
+              100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+        'gamma': [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009,
+                  0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009,
+                  0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                  0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+                  1, 2, 3, 4, 5, 6, 7, 8, 9,
+                  10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
 
-    # X_resampled, y_resampled = undersampling_nearmiss(
-    #     X, y, version=3, n_neighbors=10)
     X_resampled, y_resampled = undersampling_clustercentroids(X, y)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X_resampled, y_resampled, test_size=0.25, random_state=42)
 
-    scaler = MinMaxScaler()
-    X_train = pd.DataFrame(scaler.fit_transform(
-        X_train), columns=X_train.columns, index=X_train.index)
-    X_test = pd.DataFrame(scaler.transform(
-        X_test), columns=X_test.columns, index=X_test.index)
+    # scaler = MinMaxScaler()
+    # X_train = pd.DataFrame(scaler.fit_transform(
+    #     X_train), columns=X_train.columns, index=X_train.index)
+    # X_test = pd.DataFrame(scaler.transform(
+    #     X_test), columns=X_test.columns, index=X_test.index)
 
     result = tune_train_evaluate_svm(
-        X_train, y_train, X_test, y_test, initial_param_grid,
+        X_train, y_train, X_test, y_test, param_grid,
         resampling_method='Cluster Centroids')
 
     # Step 6: Train the final model with the best hyperparameters and evaluate it
     classifier, evaluation_metrics = train_evaluate_final_svm(
         X_train, y_train, X_test, y_test, result['best_params']
     )
-    # classifier, evaluation_metrics = train_evaluate_final_svm(
-    #     X_train, y_train, X_test, y_test, {
-    #         'C': 100.0, 'gamma': 0.03}
-    # )
 
     return feature_list, classifier, evaluation_metrics

scripts/svm/interpretation_shap.py
@@ -4,6 +4,8 @@ Created on Wed May  7 14:44:30 2025
 
 @author: Christian
 """
+from IPython.display import display
+import shap
 import time
 import pandas as pd
 import numpy as np
@@ -61,20 +63,184 @@ if __name__ == '__main__':
     # -------------------------------------------------------
     # STABILITY VARYING C AND GAMMA
     # -------------------------------------------------------
-    SHAP_16 = ['TaG_delta_5d',
-               'TminG_delta_3d',
-               'HS_delta_5d',
-               'WetSnow_Temperature',
-               'New_MF_Crust',
-               'Precip_3d',
-               'Precip_2d',
-               'TempGrad_HS',
-               'Tsnow_delta_3d',
-               'TmaxG_delta_3d',
-               'HSnum',
-               'TempAmplitude_2d',
-               'WetSnow_CS',
-               'TaG',
-               'Tsnow_delta_2d',
-               'DayOfSeason']
-    res_shap16 = evaluate_svm_with_feature_selection(mod1, SHAP_16)
+    feature_list = ['TaG_delta_5d',
+                    'TminG_delta_3d',
+                    'HS_delta_5d',
+                    # 'WetSnow_Temperature',
+                    'New_MF_Crust',
+                    'Precip_3d',
+                    'Precip_2d',
+                    'TempGrad_HS',
+                    'Tsnow_delta_3d',
+                    'TmaxG_delta_3d',
+                    'HSnum',
+                    'TempAmplitude_2d',
+                    # 'WetSnow_CS',
+                    'TaG',
+                    'Tsnow_delta_2d',
+                    'DayOfSeason']
+    res_shap16 = evaluate_svm_with_feature_selection(mod1, feature_list)
+
+# Add target variable to the feature list
+feature_with_target = feature_list + ['AvalDay']
+
+# Data preprocessing: filter relevant features and drop missing values
+clean_data = mod1[feature_with_target].dropna()
+
+# Extract features and target variable
+X = clean_data[feature_list]
+y = clean_data['AvalDay']
+
+features_to_remove = remove_correlated_features(X, y)
+
+X = X.drop(columns=features_to_remove)
+
+X_resampled, y_resampled = undersampling_clustercentroids(X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_resampled, y_resampled, test_size=0.25, random_state=42)
+
+scaler = MinMaxScaler()
+X_train = pd.DataFrame(scaler.fit_transform(
+    X_train), columns=X_train.columns, index=X_train.index)
+X_test = pd.DataFrame(scaler.transform(
+    X_test), columns=X_test.columns, index=X_test.index)
+
+# Step 6: Train the final model with the best hyperparameters and evaluate it
+classifier, evaluation_metrics = train_evaluate_final_svm(
+    X_train, y_train, X_test, y_test, {'C': 200, 'gamma': 0.3})
+
+# Use SHAP KernelExplainer for non-tree-based models like SVM
+model = svm.SVC(C=200, gamma=0.3, probability=True, random_state=42)
+model.fit(X_train, y_train)
+
+explainer = shap.KernelExplainer(model.predict_proba, X_train)
+
+# Compute SHAP values for the test set
+shap_values = explainer.shap_values(X_test)
+
+shap_values_class1 = shap_values[:, :, 1]
+shap.summary_plot(shap_values_class1, X_test)
+
+shap.dependence_plot("WetSnow_Temperature", shap_values_class1, X_test)
+
+shap_exp = shap.Explanation(
+    values=shap_values[:, :, 1],       # SHAP values for class 1
+    base_values=explainer.expected_value[1],  # Base value for class 1
+    data=X_test.values,                # Feature values
+    feature_names=X_test.columns       # Column names
+)
+
+y_test_np = y_test.to_numpy() if hasattr(
+    y_test, "to_numpy") else np.array(y_test)
+
+# Now create cohorts using boolean indexing
+cohorts = {
+    "No Avalanche": shap_exp[y_test_np == 0],
+    "Avalanche": shap_exp[y_test_np == 1]
+}
+shap.plots.bar(cohorts, max_display=16)
+
+shap.summary_plot(cohorts["Avalanche"], X_test[y_test == 1])
+shap.summary_plot(cohorts["No Avalanche"], X_test[y_test == 0])
+
+# Replace 'shap_values' with the correct key if needed
+shap.plots.violin(cohorts['Avalanche'])
+# Replace 'shap_values' with the correct key if needed
+shap.plots.violin(cohorts['No Avalanche'])
+
+# for i in range(len(shap_exp)):  # Loop through the indices of shap_exp
+#     shap.plots.waterfall(shap_exp[i])
+
+
+# ✅ 1. Global Feature Importance (Summary Plot)
+shap.summary_plot(shap_values[:, :, 1], X_test)
+
+clustering = shap.utils.hclust(X_test, y_test)
+shap.plots.bar(shap_values[:, :, 1], clustering=clustering)
+
+# Crea Explanation object (classe positiva: 1)
+explanation1 = shap.Explanation(
+    values=shap_values[:, :, 1],
+    base_values=explainer.expected_value[1],
+    data=X_test,
+    feature_names=X_test.columns
+)
+
+explanation0 = shap.Explanation(
+    values=shap_values[:, :, 0],
+    base_values=explainer.expected_value[0],
+    data=X_test,
+    feature_names=X_test.columns
+)
+
+
+# Ora puoi usare il bar plot
+shap.plots.bar(explanation1, max_display=16)
+shap.plots.bar(explanation0, max_display=16)
+
+
+# ✅ 2. Summary Bar Plot
+shap.summary_plot(shap_values[:, :, 1], X_test, plot_type="bar")
+
+# ✅ 3. Force Plot (Local Explanation for One Prediction)
+i = 0  # Index of the sample to explain
+shap.initjs()  # Enable JS visualizations
+
+shap.force_plot(
+    explainer.expected_value[1],   # base value for class 1
+    shap_values[i, :, 1],          # SHAP values for instance i and class 1
+    X_test.iloc[i]                 # Feature values for instance i
+)
+
+
+force_plot = shap.force_plot(
+    explainer.expected_value[1],
+    shap_values[i, :, 1],
+    X_test.iloc[i]
+)
+display(force_plot)  # <- This makes the plot appear in the notebook
+# Create the force plot
+force_plot = shap.force_plot(
+    explainer.expected_value[1],
+    shap_values[10, :, 1],
+    X_test.iloc[i]
+)
+
+# Save it to an HTML file
+shap.save_html("shap_force_plot.html", force_plot)
+
+
+# ✅ 4. Decision Plot
+shap.decision_plot(
+    explainer.expected_value[1],
+    shap_values[:2, :, 1],        # first 5 samples
+    X_test.iloc[:2]
+)
+
+# ✅ 5. Dependence Plot
+shap.dependence_plot(
+    "Precip_3d",               # replace with a column name from X_test
+    shap_values[:, :, 1],
+    X_test
+)
+
+shap.dependence_plot(
+    "Precip_3d",
+    shap_values[:, :, 1],
+    X_test,
+    interaction_index="auto"
+)
+
+# ✅ 6. Waterfall Plot
+shap.plots._waterfall.waterfall_legacy(
+    explainer.expected_value[1],
+    shap_values[96, :, 1],
+    X_test.iloc[96]
+)
+
+
+# shap_values_df = pd.DataFrame(
+#     shap_values[:, :, 1], columns=X_train.columns)
+
+# shap.plots.bar(shap_values)

scripts/svm/statistics_EDA.py
@@ -39,6 +39,57 @@ from scripts.svm.utils import (save_outputfile, get_adjacent_values, Permutation
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 
+from scripts.svm.eda_analysis import run_eda
+
+# -------------------------------------------
+# FUNZIONI UTILI
+# -------------------------------------------
+
+
+def load_data(filepath):
+    df = pd.read_csv(filepath)
+    return df
+
+
+def remove_correlated_features(X, y, threshold=0.9):
+    corr_matrix = X.corr().abs()
+    upper = corr_matrix.where(
+        np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+    to_drop = [column for column in upper.columns if any(
+        upper[column] > threshold)]
+    return to_drop
+
+
+def undersampling_clustercentroids(X, y):
+    cc = ClusterCentroids(random_state=42)
+    X_resampled, y_resampled = cc.fit_resample(X, y)
+    return pd.DataFrame(X_resampled, columns=X.columns), pd.Series(y_resampled, name='AvalDay')
+
+
+def compare_distributions(original, resampled, features, n_plot=5):
+    ks_results = []
+
+    for i, col in enumerate(features):
+        orig_values = original[col]
+        res_values = resampled[col]
+
+        ks_stat, p_value = ks_2samp(orig_values, res_values)
+        ks_results.append((col, ks_stat, p_value))
+
+        if i < n_plot:
+            plt.figure(figsize=(6, 3))
+            sns.kdeplot(orig_values, label='Original', fill=True)
+            sns.kdeplot(res_values, label='Resampled', fill=True)
+            plt.title(f'Distribuzione: {col} (p={p_value:.3f})')
+            plt.legend()
+            plt.tight_layout()
+            plt.show()
+
+    ks_df = pd.DataFrame(ks_results, columns=[
+                         'Feature', 'KS_statistic', 'p_value'])
+    ks_df['Significant_change'] = ks_df['p_value'] < 0.05
+    return ks_df.sort_values(by='KS_statistic', ascending=False)
+
 
 if __name__ == '__main__':
 
@@ -62,16 +113,78 @@ if __name__ == '__main__':
     mod1 = load_data(filepath)
     print(mod1.dtypes)  # For initial data type
 
-    # --- EXPLORATORY DATA ANALYSIS ---
+    # --- AFTER RESAMPLING ---
+    feature = ['TaG', 'TminG', 'TmaxG', 'HSnum',
+           'HNnum', 'TH01G', 'TH03G', 'PR', 'DayOfSeason', 'HS_delta_1d',
+           'HS_delta_2d', 'HS_delta_3d', 'HS_delta_5d', 'HN_2d', 'HN_3d', 'HN_5d',
+           'DaysSinceLastSnow', 'Tmin_2d', 'Tmax_2d', 'Tmin_3d', 'Tmax_3d',
+           'Tmin_5d', 'Tmax_5d', 'TempAmplitude_1d', 'TempAmplitude_2d',
+           'TempAmplitude_3d', 'TempAmplitude_5d', 'TaG_delta_1d', 'TaG_delta_2d',
+           'TaG_delta_3d', 'TaG_delta_5d', 'TminG_delta_1d', 'TminG_delta_2d',
+           'TminG_delta_3d', 'TminG_delta_5d', 'TmaxG_delta_1d', 'TmaxG_delta_2d',
+           'TmaxG_delta_3d', 'TmaxG_delta_5d', 'T_mean', 'DegreeDays_Pos',
+           'DegreeDays_cumsum_2d', 'DegreeDays_cumsum_3d', 'DegreeDays_cumsum_5d',
+           'FreshSWE', 'SeasonalSWE_cum', 'Precip_1d', 'Precip_2d', 'Precip_3d',
+           'Precip_5d', 'Penetration_ratio',
+           # 'WetSnow_CS', 'WetSnow_Temperature',
+           'TempGrad_HS', 'TH10_tanh', 'TH30_tanh', 'Tsnow_delta_1d',
+           'Tsnow_delta_2d', 'Tsnow_delta_3d', 'Tsnow_delta_5d',
+           'ConsecWetSnowDays', 'MF_Crust_Present',
+           'New_MF_Crust', 'ConsecCrustDays']
 
-    # COUNTS
-    mod1['AvalDay'].value_counts().plot(kind='bar')
-    plt.title("Class distribution: Avalanche vs No Avalanche")
-    plt.xticks([0, 1], ['No Avalanche', 'Avalanche'], rotation=0)
-    plt.show()
+    feature_plus = feature + ['AvalDay']
+    mod1_clean = mod1[feature_plus]
+    mod1_clean = mod1_clean.dropna()
 
-    # UNIVARIATE ANALYSIS
-    for col in mod1.columns[1:]:
+    X = mod1_clean[feature]
+    y = mod1_clean['AvalDay']
+    mod1_clean = mod1_clean.dropna()
+
+    features_correlated = remove_correlated_features(X, y)
+    X_new = X.drop(columns=features_correlated)
+
+    X_resampled, y_resampled = undersampling_clustercentroids(X_new, y)
+
+    df_resampled = X_resampled
+    df_resampled['AvalDay'] = y_resampled
+
+    # -------------------------------------------
+# CONFRONTO DISTRIBUZIONI ORIGINALE VS RESAMPLING
+# -------------------------------------------
+
+print("\n--- Confronto delle distribuzioni (originale vs resampled) ---")
+comparison_df = compare_distributions(
+    X_new, X_resampled, X_new.columns, n_plot=10)
+print(comparison_df)
+
+    # --- EXPLORATORY DATA ANALYSIS BEFORE RESAMPLING ---
+    # Lista di feature che dovrebbero essere binarie
+# aggiorna in base al tuo caso
+binary_features = ['New_MF_Crust', 'MF_Crust_Present']
+
+run_eda(mod1_clean, title='Original Data',
+        binary_features_to_check=binary_features)
+run_eda(df_resampled, title='Resampled Data (ClusterCentroids)',
+        binary_features_to_check=binary_features)
+
+
+# Lista di feature che dovrebbero essere binarie
+# aggiorna in base al tuo caso
+binary_features = ['WetSnow_temperature', 'ConsecWetSnowDays']
+
+run_eda(mod1_clean, title='Original Data',
+        binary_features_to_check=binary_features)
+run_eda(df_resampled, title='Resampled Data (ClusterCentroids)',
+        binary_features_to_check=binary_features)
+
+# COUNTS
+mod1['AvalDay'].value_counts().plot(kind='bar')
+plt.title("Class distribution: Avalanche vs No Avalanche")
+ plt.xticks([0, 1], ['No Avalanche', 'Avalanche'], rotation=0)
+  plt.show()
+
+   # UNIVARIATE ANALYSIS
+   for col in mod1.columns[1:]:
         plt.figure(figsize=(6, 3))
         sns.kdeplot(data=mod1, x=col, hue='AvalDay',
                     fill=True, common_norm=False, alpha=0.5)
@@ -252,76 +365,58 @@ if __name__ == '__main__':
     accuracy = accuracy_score(y, y_pred)
     print(f'Accuracy: {accuracy}')
 
-    # KMEANS clustering
+    # --- EXPLORATORY DATA ANALYSIS AFTER RESAMPLING ---
+    feature = [
+        'TaG', 'TminG', 'TmaxG', 'HSnum',
+        'HNnum', 'TH01G', 'TH03G', 'PR', 'DayOfSeason',
+        'HS_delta_1d', 'HS_delta_2d', 'HS_delta_3d', 'HS_delta_5d',
+        'HN_2d', 'HN_3d', 'HN_5d',
+        'DaysSinceLastSnow', 'Tmin_2d', 'Tmax_2d', 'Tmin_3d', 'Tmax_3d',
+        'Tmin_5d', 'Tmax_5d', 'TempAmplitude_1d', 'TempAmplitude_2d',
+        'TempAmplitude_3d', 'TempAmplitude_5d', 'TaG_delta_1d', 'TaG_delta_2d',
+        'TaG_delta_3d', 'TaG_delta_5d', 'TminG_delta_1d', 'TminG_delta_2d',
+        'TminG_delta_3d', 'TminG_delta_5d', 'TmaxG_delta_1d', 'TmaxG_delta_2d',
+        'TmaxG_delta_3d', 'TmaxG_delta_5d', 'T_mean', 'DegreeDays_Pos',
+        'DegreeDays_cumsum_2d', 'DegreeDays_cumsum_3d', 'DegreeDays_cumsum_5d',
+        'Precip_1d', 'Precip_2d', 'Precip_3d', 'Precip_5d',
+        'Penetration_ratio',
+        'TempGrad_HS', 'TH10_tanh', 'TH30_tanh', 'Tsnow_delta_1d',
+        'Tsnow_delta_2d', 'Tsnow_delta_3d',
+        'Tsnow_delta_5d', 'ConsecWetSnowDays']
 
-    # Prepare your data
-X = df_corr.drop(columns='AvalDay')  # or whatever your features are
-X = X.dropna()
-X_scaled = StandardScaler().fit_transform(X)  # Standardize features
+    feature_plus = feature + ['AvalDay']
+    mod1_clean = mod1[feature_plus]
+    mod1_clean = mod1_clean.dropna()
 
-# Apply K-Means
-kmeans = KMeans(n_clusters=3, random_state=42)
-kmeans.fit(X_scaled)
+    X = mod1_clean[feature]
+    y = mod1_clean['AvalDay']
+    mod1_clean = mod1_clean.dropna()
 
-# Add the cluster labels to your DataFrame (optional)
-df_corr['Cluster'] = kmeans.labels_
+    features_correlated = remove_correlated_features(X, y)
+    X_new = X.drop(columns=features_correlated)
 
-# Plot clusters (only works if X has 2D/3D after PCA or t-SNE)
-plt.figure(figsize=(8, 6))
-plt.scatter(X_scaled[:, 0], X_scaled[:, 1],
-            c=kmeans.labels_, cmap='viridis', edgecolor='k')
-plt.title("K-Means Clustering")
-plt.xlabel("Feature 1")
-plt.ylabel("Feature 2")
-plt.show()
+    X_resampled, y_resampled = undersampling_clustercentroids(X_new, y)
 
-inertias = []
-for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(X_scaled)
-    inertias.append(kmeans.inertia_)
+    df_resampled = X_resampled
+    df_resampled['AvalDay'] = y_resampled
 
-plt.plot(range(1, 11), inertias, marker='o')
-plt.title('Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('Inertia')
-plt.show()
+    # COUNTS
+    df_resampled['AvalDay'].value_counts().plot(kind='bar')
+    plt.title("Class distribution: Avalanche vs No Avalanche")
+    plt.xticks([0, 1], ['No Avalanche', 'Avalanche'], rotation=0)
+    plt.show()
 
+    # UNIVARIATE ANALYSIS
+    for col in df_resampled.columns[1:]:
+        plt.figure(figsize=(6, 3))
+        sns.kdeplot(data=df_resampled, x=col, hue='AvalDay',
+                    fill=True, common_norm=False, alpha=0.5)
+        plt.title(f'Distribution of {col} by Avalanche Occurrence')
+        plt.show()
 
-from sklearn.preprocessing import StandardScaler
+      # 3. Boxplots for Class Comparison
 
-X = df_corr.drop(columns=['AvalDay', 'Cluster'])  # drop target and cluster if already present
-X_scaled = StandardScaler().fit_transform(X)
-from sklearn.decomposition import PCA
-
-pca = PCA(n_components=2)
-X_pca = pca.fit_transform(X_scaled)
-
-from sklearn.cluster import KMeans
-
-kmeans = KMeans(n_clusters=2, random_state=42)  # use 3 or 4 based on your elbow
-kmeans.fit(X_scaled)
-labels = kmeans.labels_
-
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(8, 6))
-plt.scatter(X_pca[:, 0], X_pca[:, 1], c=labels, cmap='viridis', s=50, edgecolor='k')
-plt.title('K-Means Clusters Visualized with PCA')
-plt.xlabel('PCA Component 1')
-plt.ylabel('PCA Component 2')
-plt.grid(True)
-plt.show()
-
-
-from sklearn.manifold import TSNE
-
-X_tsne = TSNE(n_components=2, perplexity=30, random_state=42).fit_transform(X_scaled)
-
-plt.figure(figsize=(8, 6))
-plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=labels, cmap='plasma', s=50, edgecolor='k')
-plt.title('K-Means Clusters Visualized with t-SNE')
-plt.xlabel('t-SNE Component 1')
-plt.ylabel('t-SNE Component 2')
-plt.grid(True)
-plt.show()
+    for col in mod1.columns[1:]:
+        sns.boxplot(x='AvalDay', y=col, data=mod1)
+        plt.title(f'{col} vs Avalanche Occurrence')
+        plt.show()